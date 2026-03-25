88c52939f72842930136e3c77a624f34ee284381
FabioHoxha1 <awsdeployment1@gmail.com>
2026-03-25 14:26:14 +0100

    Apply additional changes

--- backend/controllers/isTokenValid.js
+++ backend/controllers/isTokenValid.js
@@ -1,31 +1,23 @@
-const CustomErrorHandler = require("../errors/customErrorHandler");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userData");
+const CustomErrorHandler = require("../errors/customErrorHandler");
 
 const isTokenvalid = async (req, res) => {
   let authHeader = req.headers.authorization;
-  const token = authHeader.split(" ")[1];
 
+  // Check if authorization header exists BEFORE trying to split it
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new CustomErrorHandler(401, false);
+  }
+  
+  const token = authHeader.split(" ")[1];
+  
+  if (!token) {
+    throw new CustomErrorHandler(401, false);
   } else {
-    const tokenVerification = jwt.verify(token, process.env.SECRET_TOKEN_KEY, (err, decoded) => {
-      if (err) {
-        return false;
-      } else {
-        return true;
-      }
-    });
-    let checkIfTokenExist = await User.findOne({ verificationToken: token });
-
-    if (!checkIfTokenExist) {
-      throw new CustomErrorHandler(401, false);
-    }
-    if (!tokenVerification && !checkIfTokenExist) {
-      throw new CustomErrorHandler(401, false);
-    } else {
-      res.status(200).send(true);
-    }
+    const isVerified = jwt.verify(token, process.env.JWT_SECRET);
+    const { userId, username } = isVerified;
+    res.status(200).json({ isTokenValid: true, userId, username });
   }
 };
+
 module.exports = isTokenvalid;
